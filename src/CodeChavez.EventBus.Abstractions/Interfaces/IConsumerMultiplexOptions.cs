815e225a1af59f3fdb64d8f22434343c937be7d8
CaiusAllester <6989623+codechavez@users.noreply.github.com>
2025-10-01 16:10:17 -0400

    Adding IConsumerMultiplexOptions

---
+++ src/CodeChavez.EventBus.Abstractions/Interfaces/IConsumerMultiplexOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace CodeChavez.EventBus.Abstractions.Interfaces;
+
+public interface IConsumerMultiplexOptions
+{
+    int MaxConcurrency { get; set; }
+
+    int PollTimeout { get; set; }
+
+    int MaxParallelism { get; set; }
+}
